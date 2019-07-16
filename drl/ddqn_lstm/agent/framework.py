12a3adbe093cb320085d8da6c770d89e5b2d14a5
mmmaaaggg <mmmaaaggg@163.com>
2019-07-16 18:46:13 +0800

    update files

--- drl/ddqn_lstm/agent/framework.py
+++ drl/ddqn_lstm/agent/framework.py
@@ -29,6 +29,8 @@ class Framework(object):
         self.cache = deque(maxlen=memory_size)
         self.weights = None  # 用于 _get_samples 基于权重提取数据
         self.logger = logging.getLogger(str(self.__class__))
+        K.clear_session()
+        tf.reset_default_graph()
 
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.3
