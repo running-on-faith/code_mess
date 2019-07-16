5bd902be7c64ec489c2d9d075c15dcbeae76e440
mmmaaaggg <mmmaaaggg@163.com>
2019-07-16 16:44:03 +0800

    add ddqn_lstm

---
+++ drl/ddqn_lstm/agent/framework.py
@@ -0,0 +1,120 @@
+import logging
+from collections import deque
+import random
+import numpy as np
+import tensorflow as tf
+from ibats_utils.mess import sample_weighted
+from keras import backend as K
+from keras.layers import Dense, LSTM, Dropout, Input
+from keras.models import Model
+from keras.optimizers import Adam
+
+_EPSILON = 1e-6  # avoid nan
+
+
+class Framework(object):
+    def __init__(self, memory_size=2048, input_shape=[None, 50, 58, 5], max_grad_norm=10, action_size=3, batch_size=128,
+                 gamma=0.95, learning_rate=0.001):
+
+        self.memory_size = memory_size
+        self.input_shape = input_shape
+        self.action_size = action_size
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.learning_rate = learning_rate
+
+        # cache for experience replay
+        self.cache = deque(maxlen=memory_size)
+        self.weights = None  # 用于 _get_samples 基于权重提取数据
+        self.logger = logging.getLogger(str(self.__class__))
+
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.3
+        self.epsilon_decay = 0.9998
+        self.model = self._build_model()
+        self.target_model = self._build_model()
+        self.update_target_net()
+
+    def _huber_loss(self, y_true, y_pred, clip_delta=1.0):
+        error = y_true - y_pred
+        cond = K.abs(error) <= clip_delta
+
+        squared_loss = 0.5 * K.square(error)
+        quadratic_loss = 0.5 * K.square(clip_delta) + clip_delta * (K.abs(error) - clip_delta)
+
+        return K.mean(tf.where(cond, squared_loss, quadratic_loss))
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        input = Input(batch_shape=self.input_shape)
+        lstm = LSTM(80, return_sequences=True)(input)
+        lstm = LSTM(40, return_sequences=False)(lstm)
+        merge = Dense(200)(lstm)
+        merge = Dropout(0.5)(merge)
+        merge = Dense(80)(merge)
+        merge = Dropout(0.5)(merge)
+        merge = Dense(self.action_size)(merge)
+
+        model = Model(input, outputs=merge)
+        model.summary()
+        model.compile(Adam(self.learning_rate), loss=self._huber_loss)
+        return model
+
+    # get random sample from experience pool
+    def _get_samples(self):
+        cache_len = len(self.cache)
+        samples_count = cache_len if len(self.cache) < self.batch_size else self.batch_size
+        # samples = random.sample(self.cache, samples_count)
+        if self.weights is None:
+            self.weights = np.ones(cache_len)
+        elif len(self.weights) < cache_len:
+            self.weights = np.concatenate([self.weights / 2, np.ones(cache_len - len(self.weights))])
+        samples = sample_weighted(self.cache, self.weights, samples_count)
+        state = np.vstack([i[0] for i in samples])
+        action = np.squeeze(np.vstack([i[1] for i in samples]))
+        reward = np.squeeze(np.vstack([i[2] for i in samples]))
+        next_state = np.vstack([i[3] for i in samples])
+        done = [i[4] for i in samples]
+        return state, action, reward, next_state, done
+
+    def get_deterministic_policy_batch(self, inputs):
+        act_values = self.model.predict(inputs)
+        return np.argmax(act_values, axis=1)
+
+    def get_deterministic_policy(self, inputs):
+        act_values = self.model.predict(inputs)
+        return np.argmax(act_values[0])  # returns action
+
+    def get_stochastic_policy(self, inputs):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(inputs)
+        return np.argmax(act_values[0])  # returns action
+
+    # update target network params
+    def update_target_net(self):
+        # copy weights from model to target_model
+        self.target_model.set_weights(self.model.get_weights())
+
+    # update experience replay pool
+    def update_cache(self, state, action, reward, next_state, done):
+        self.cache.append((state, action, reward, next_state, done))
+
+    # train, update value network params
+    def update_value_net(self):
+        state, action, reward, next_state, done = self._get_samples()
+        target = self.model.predict(state)
+        t = self.target_model.predict(state)
+        done_arr = 1 - np.array(done).astype('int')
+        target[np.arange(target.shape[0]), action] = reward + done_arr * self.gamma * np.amax(t, axis=1)
+
+        # if done:
+        #     target[0][action] = reward
+        # else:
+        #     # a = self.model.predict([next_state,matrix])[0]
+        #     t = self.target_model.predict([next_state, matrix])[0]
+        #     target[0][action] = reward + self.gamma * np.amax(t)
+        #     # target[0][action] = reward + self.gamma * t[np.argmax(a)]
+        self.model.fit(state, target, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
---
+++ drl/ddqn_lstm/agent/main.py
@@ -0,0 +1,257 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2019/6/28 17:19
+@File    : __init__.py.py
+@contact : mmmaaaggg@163.com
+@desc    :
+需要安装相关包
+pip install graphs dm-sonnet==1.19
+https://github.com/deepmind/sonnet
+dm-sonnet==1.19 对应 tensorflow==1.5.1
+"""
+import logging
+
+import tensorflow as tf
+
+from ibats_common.example.drl.ddqn_lstm.agent.framework import Framework
+
+
+class Agent(object):
+    def __init__(self, input_shape=None, **kwargs):
+        tf.reset_default_graph()
+        if input_shape is not None:
+            input_shape = list(input_shape)
+            input_shape[0] = None
+            self.agent = Framework(input_shape=input_shape, **kwargs)
+        else:
+            self.agent = Framework(**kwargs)
+
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+
+    def choose_action_deterministic(self, inputs):
+        return self.agent.get_deterministic_policy(inputs)
+
+    def choose_action_stochastic(self, inputs):
+        return self.agent.get_stochastic_policy(inputs)
+
+    def update_cache(self, state, action, reward, next_state, done):
+        self.agent.update_cache(state, action, reward, next_state, done)
+
+    def update_eval(self):
+        self.agent.update_value_net()
+
+    def update_target(self):
+        self.agent.update_target_net()
+
+    def save_model(self, path="model/weights.h5"):
+        # return self.saver.save(self.sess, path)
+        return self.agent.model.save_weights(path)
+
+    def restore_model(self, path="model/weights.h5"):
+        # self.saver.restore(self.sess, path)
+        self.agent.model.load_weights(path)
+
+    def close(self):
+        # self.sess.close()
+        pass
+
+
+def _test_agent():
+    import pandas as pd
+    # 建立相关数据
+    n_step = 60
+    ohlcav_col_name_list = ["open", "high", "low", "close", "amount", "volume"]
+    from ibats_common.example.data import load_data
+    md_df = load_data('RB.csv').set_index('trade_date')[ohlcav_col_name_list]
+    md_df.index = pd.DatetimeIndex(md_df.index)
+    from ibats_common.backend.factor import get_factor, transfer_2_batch
+    factors_df = get_factor(md_df, dropna=True)
+    df_index, df_columns, data_arr_batch = transfer_2_batch(factors_df, n_step=n_step)
+    md_df = md_df.loc[df_index, :]
+
+    from ibats_common.backend.rl.emulator.account import Account
+    env = Account(md_df, data_arr_batch)
+    agent = Agent()
+    # fill cache
+    for episode in range(2):
+        state = env.reset()
+        while True:
+            action = agent.choose_action_stochastic(state)
+            next_state, reward, done = env.step(action)
+            agent.update_cache(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                break
+    print(len(agent.agent.cache))
+    reward_df = env.plot_data()
+    reward_df.to_csv('reward_df.csv')
+
+
+def train(md_df, batch_factors, round_n=None):
+    import pandas as pd
+    from ibats_common.backend.rl.emulator.account import Account
+    logger = logging.getLogger(__name__)
+    env = Account(md_df, data_factors=batch_factors)
+    agent = Agent(input_shape=batch_factors.shape, gamma=0.3)
+    num_episodes, n_episode_pre_record = 200, 20
+
+    target_step_size = 512
+    train_step_size = 32
+
+    episodes_train = []
+    global_step = 0
+    for episode in range(num_episodes):
+        state = env.reset()
+        episode_step = 0
+        while True:
+            global_step += 1
+            episode_step += 1
+
+            action = agent.choose_action_stochastic(state)
+            next_state, reward, done = env.step(action)
+            agent.update_cache(state, action, reward, next_state, done)
+            state = next_state
+
+            if global_step % target_step_size == 0:
+                agent.update_target()
+                # print('global_step=%d, episode_step=%d, agent.update_target()' % (global_step, episode_step))
+
+            if episode_step % train_step_size == 0 or done:
+                agent.update_eval()
+                # print('global_step=%d, episode_step=%d, agent.update_eval()' % (global_step, episode_step))
+
+                if done:
+                    # print("episode=%d, data_observation.shape[0]=%d, env.A.total_value=%f" % (
+                    #     episode, env.A.data_observation.shape[0], env.A.total_value))
+                    if episode % n_episode_pre_record == 0 or episode == num_episodes - 1:
+                        if round_n is None:
+                            logger.debug("episode=%d, data_observation.shape[0]=%d, env.A.total_value=%f",
+                                         episode, env.A.data_observation.shape[0], env.A.total_value)
+                        else:
+                            logger.debug("round=%d, episode=%3d, env.A.total_value=%f",
+                                         round_n, episode, env.A.total_value)
+                        episodes_train.append(env.plot_data())
+                    break
+
+    import matplotlib.pyplot as plt
+    reward_df = env.plot_data()
+    reward_df.iloc[:, 0].plot()  # figsize=(16, 6)
+    import datetime
+    from ibats_utils.mess import datetime_2_str
+    dt_str = datetime_2_str(datetime.datetime.now(), '%Y-%m-%d %H_%M_%S')
+    title = f'd3qn3_gama_{agent.agent.gamma:.1f}_{dt_str}'
+    plt.suptitle(title)
+    plt.show()
+    value_df = pd.DataFrame({num: df['value']
+                             for num, df in enumerate(episodes_train, start=1)
+                             if df.shape[0] > 0})
+    value_df.plot()
+    title += ' all'
+    plt.suptitle(title)
+    from ibats_common.analysis.plot import plot_or_show
+    plot_or_show(enable_save_plot=True, enable_show_plot=True, file_name=f'train_{title}.png')
+
+    if reward_df.iloc[-1, 0] > reward_df.iloc[0, 0]:
+        path = agent.save_model()
+        logger.debug('model save to path: %s', path)
+    agent.close()
+    return reward_df
+
+
+def _test_agent2():
+    import pandas as pd
+    logger = logging.getLogger(__name__)
+    # 建立相关数据
+    n_step = 250
+    ohlcav_col_name_list = ["open", "high", "low", "close", "amount", "volume"]
+    from ibats_common.example.data import load_data
+    md_df = load_data('RB.csv').set_index('trade_date')[ohlcav_col_name_list]
+    md_df.index = pd.DatetimeIndex(md_df.index)
+    from ibats_common.backend.factor import get_factor, transfer_2_batch
+    factors_df = get_factor(md_df, dropna=True)
+    df_index, df_columns, data_arr_batch = transfer_2_batch(factors_df, n_step=n_step)
+    batch_factors = data_arr_batch
+    # shape = [data_arr_batch.shape[0], 5, int(n_step / 5), data_arr_batch.shape[2]]
+    # batch_factors = np.transpose(data_arr_batch.reshape(shape), [0, 2, 3, 1])
+    # print(data_arr_batch.shape, '->', shape, '->', batch_factors.shape)
+    md_df = md_df.loc[df_index, :]
+
+    success_count, success_max_count, round_n = 0, 10, 0
+    while True:
+        round_n += 1
+        df = train(md_df, batch_factors, round_n=round_n)
+        logger.debug(df.iloc[-1, :])
+        if df["value"].iloc[-1] > df["value"].iloc[0]:
+            success_count += 1
+            logger.debug('is win %d/%d' % (success_count, success_max_count))
+            if success_count >= success_max_count:
+                break
+
+
+def load_predict(md_df, data_factors, tail_n=1, show_plot=True, model_path="model/ddqn.ckpt"):
+    import pandas as pd
+    from ibats_common.backend.rl.emulator.account import Account
+    if tail_n is not None and tail_n > 0:
+        states = data_factors[-tail_n:]
+        md_df = md_df.iloc[-tail_n:]
+    else:
+        states = data_factors
+
+    env = Account(md_df, data_factors=data_factors)
+    agent = Agent(input_shape=data_factors.shape)
+    agent.restore_model(path=model_path)
+    print("加载模型：%s 完成" % model_path)
+    actions = []
+    for num in range(states.shape[0]):
+        state = states[num:num + 1]
+        action = agent.choose_action_deterministic(state)
+        actions.append(action)
+        next_state, reward, done = env.step(action)
+        if done:
+            print('执行循环 %d 次' % num)
+            break
+
+    action_df = pd.DataFrame({'action': actions}, index=md_df.index[:states.shape[0] - 1])
+    print('action_df', action_df)
+    if show_plot:
+        import matplotlib.pyplot as plt
+        reward_df = env.plot_data()
+        value_s = reward_df.iloc[:, 0]
+        value_s.plot()  # figsize=(16, 6)
+        from ibats_utils.mess import datetime_2_str
+        from datetime import datetime
+        plt.suptitle(datetime_2_str(datetime.now()))
+        plt.show()
+    else:
+        reward_df = env.plot_data()
+
+    return reward_df
+
+
+def _test_load_predict():
+    import pandas as pd
+    # 建立相关数据
+    n_step = 250
+    ohlcav_col_name_list = ["open", "high", "low", "close", "amount", "volume"]
+    from ibats_common.example.data import load_data
+    md_df = load_data('RB.csv').set_index('trade_date')[ohlcav_col_name_list]
+    md_df.index = pd.DatetimeIndex(md_df.index)
+    from ibats_common.backend.factor import get_factor, transfer_2_batch
+    factors_df = get_factor(md_df, dropna=True)
+    df_index, df_columns, data_arr_batch = transfer_2_batch(factors_df, n_step=n_step)
+    data_factors, shape = data_arr_batch, data_arr_batch.shape
+    # shape = [data_arr_batch.shape[0], 5, int(n_step / 5), data_arr_batch.shape[2]]
+    # data_factors = np.transpose(data_arr_batch.reshape(shape), [0, 2, 3, 1])
+    # print(data_arr_batch.shape, '->', shape, '->', data_factors.shape)
+    md_df = md_df.loc[df_index, :]
+    model_path = r'/home/mg/github/IBATS_Common/ibats_common/tf_saves_2019-07-08_18_00_57/model_tfls/2014-09-22/model_dqn_0.tfl'
+    load_predict(md_df, data_factors, tail_n=0, model_path=model_path)
+
+
+if __name__ == '__main__':
+    # _test_agent()
+    _test_agent2()
+    # _test_load_predict()
