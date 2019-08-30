f923803febfad6ada4f1be2dd45bbcc12692d03d
mmmaaaggg <mmmaaaggg@163.com>
2019-08-30 17:15:37 +0800

    sub test

---
+++ drl/d3qn_replay_2019_08_17/agent/new_test/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 19-8-22 上午8:48
+@File    : __init__.py.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+from ibats_common.example.drl.d3qn_replay_2019_08_17.agent.main import _test_agent2
+
+
+if __name__ == "__main__":
+    _test_agent2(round_from=2, increase=500)
