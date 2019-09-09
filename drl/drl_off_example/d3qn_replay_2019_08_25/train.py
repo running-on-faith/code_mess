ad13dd4f87189eab1545cb9af8f5fe46cb84ea08
mmmaaaggg <mmmaaaggg@163.com>
2019-09-09 18:00:19 +0800

    add train_round_kwargs_iter

--- drl/drl_off_example/d3qn_replay_2019_08_25/train.py
+++ drl/drl_off_example/d3qn_replay_2019_08_25/train.py
@@ -11,9 +11,33 @@
 #     from ibats_common.backend.rl.utils import use_cup_only
 #
 #     use_cup_only()
-
-from drl.trainer import train_on_each_period
+import math
 from drl.d3qn_replay_2019_08_25.agent.main import MODEL_NAME, get_agent
+from drl.trainer import train_on_each_period
+import logging
+
+
+def train_round_iter_func(round_n_per_target_day, target_avg_holding_days=[4, 5, 7]):
+    # 作为训练起始随机动作时，平均换仓天数，该参数可能导致训练后的模型调仓频率变化
+    round_n = 1
+    for round_n_sub in range(round_n_per_target_day):
+        for days in target_avg_holding_days:
+            env_kwargs = dict(state_with_flag=True, fee_rate=0.001)
+            agent_kwargs = dict(keep_last_action=math.pow(0.5, 1 / days), batch_size=512, epsilon_memory_size=20)
+            num_episodes = 2000 + 200 * round_n_sub
+            train_kwargs = dict(round_n=round_n, num_episodes=num_episodes, n_episode_pre_record=int(num_episodes / 6),
+                                model_name=MODEL_NAME, get_agent_func=get_agent)
+            yield round_n, env_kwargs, agent_kwargs, train_kwargs
+            round_n += 1
+
+
+def _test_train_round_iter_func(round_n_per_target_day=3):
+    logger = logging.getLogger(__name__)
+    for round_n, env_kwargs, agent_kwargs, train_kwargs in train_round_iter_func(round_n_per_target_day):
+        logger.error("round_n=%d, env_kwargs=%s, agent_kwargs%s, train_kwargs%s",
+                     round_n, env_kwargs, agent_kwargs, train_kwargs)
+
 
 if __name__ == '__main__':
-    train_on_each_period(model_name=MODEL_NAME, get_agent_func=get_agent, round_from=0, round_max=4)
+    train_on_each_period(train_round_iter_func(round_n_per_target_day=2), n_step=60)
+    # _test_train_round_iter_func(round_n_per_target_day=2)
