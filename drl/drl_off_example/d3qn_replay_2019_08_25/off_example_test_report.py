5e6995eba9ee6567bdf0d547e0af3890fbde92cd
mmmaaaggg <mmmaaaggg@163.com>
2019-09-26 17:15:08 +0800

    rename functions

--- drl/drl_off_example/d3qn_replay_2019_08_25/off_example_test_report.py
+++ drl/drl_off_example/d3qn_replay_2019_08_25/off_example_test_report.py
@@ -19,7 +19,7 @@ from drl.d3qn_replay_2019_08_25.agent.main import MODEL_NAME, get_agent
 from drl.validator import validate_bunch, auto_valid_and_report
 
 
-def valid_model(auto_open_file=True, auto_open_summary_file=True):
+def valid_models_and_summary_report(auto_open_file=True, auto_open_summary_file=True):
     round_results_dic, file_path = validate_bunch(
         md_loader=lambda range_to=None: load_data(
             'RB.csv', folder_path=DATA_FOLDER_PATH, index_col='trade_date', range_to=range_to)[OHLCAV_COL_NAME_LIST],
@@ -38,20 +38,19 @@ def valid_model(auto_open_file=True, auto_open_summary_file=True):
             open_file_with_system_app(file_path)
 
 
-def valid_whole(output_folder, auto_open_file=False, auto_open_summary_file=False):
-    auto_valid_and_report(output_folder, MODEL_NAME, get_agent,
-                          auto_open_file=auto_open_file, auto_open_summary_file=auto_open_summary_file)
-
-
-if __name__ == "__main__":
+def valid_whole_episodes_and_summary_report(auto_open_file=False, auto_open_summary_file=False):
     from ibats_utils.mess import is_windows_os
     if is_windows_os():
         output_folder = r'D:\WSPych\code_mess\drl\drl_off_example\d3qn_replay_2019_08_25\output'
     else:
         output_folder = r'/home/mg/github/code_mess/drl/drl_off_example/d3qn_replay_2019_08_25/output'
-    # valid_model()
-    valid_whole(
-        output_folder=output_folder,
+    # valid_models_and_summary_report()
+    auto_valid_and_report(output_folder, MODEL_NAME, get_agent,
+                          auto_open_file=auto_open_file, auto_open_summary_file=auto_open_summary_file)
+
+
+if __name__ == "__main__":
+    valid_whole_episodes_and_summary_report(
         auto_open_file=False,
         auto_open_summary_file=False
     )
