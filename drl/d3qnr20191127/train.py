35c146d5b3b6a0cd16901dd105ef153f2dc09141
mmmaaaggg <mmmaaaggg@163.com>
2019-11-27 11:22:55 +0800

    add comments

--- drl/d3qnr20191127/train.py
+++ drl/d3qnr20191127/train.py
@@ -19,16 +19,22 @@ from drl.d3qnr20191127.agent.main import MODEL_NAME, get_agent
 from drl.trainer import train_on_each_period
 
 
-def train_round_iter_func(round_n_per_target_day, target_avg_holding_days=[4, 6, 8]):
+def train_round_iter_func(round_n_per_target_day, target_avg_holding_days=[5, 7, 9]):
+    """
+    迭代器函数：每轮训练迭代参数 迭代器
+    :param round_n_per_target_day:
+    :param target_avg_holding_days:
+    :return:
+    """
     # 作为训练起始随机动作时，平均换仓天数，该参数可能导致训练后的模型调仓频率变化
     round_n = 1
     for round_n_sub in range(round_n_per_target_day):
         for days in target_avg_holding_days:
             env_kwargs = dict(state_with_flag=True, fee_rate=0.001)
             agent_kwargs = dict(keep_last_action=math.pow(0.5, 1 / days), batch_size=512,
-                                epsilon_memory_size=10, random_drop_best_cache_rate=0.1)
+                                epsilon_memory_size=10, random_drop_best_cache_rate=0.05)
             num_episodes = 1000 + 200 * round_n_sub
-            train_kwargs = dict(round_n=round_n, num_episodes=num_episodes, n_episode_pre_record=int(num_episodes / 6),
+            train_kwargs = dict(round_n=round_n, num_episodes=num_episodes, n_episode_pre_record=num_episodes // 8,
                                 model_name=MODEL_NAME, get_agent_func=get_agent)
             yield round_n, env_kwargs, agent_kwargs, train_kwargs
             round_n += 1
@@ -41,7 +47,7 @@ def _test_train_round_iter_func(round_n_per_target_day=3):
                      round_n, env_kwargs, agent_kwargs, train_kwargs)
 
 
-if __name__ == '__main__':
+def _test_train_on_each_period():
     from ibats_common.example.data import load_data, OHLCAV_COL_NAME_LIST
     import functools
     from ibats_common.backend.factor import get_factor
@@ -56,8 +62,13 @@ if __name__ == '__main__':
         md_loader_func=lambda range_to=None: load_data(
             'RB.csv', folder_path=DATA_FOLDER_PATH, index_col='trade_date', range_to=range_to)[OHLCAV_COL_NAME_LIST],
         get_factor_func=get_factor_func,
-        train_round_kwargs_iter_func=functools.partial(train_round_iter_func, round_n_per_target_day=2), n_step=60,
-        date_train_from='2015-09-30', offset='4M',
+        train_round_kwargs_iter_func=functools.partial(train_round_iter_func, round_n_per_target_day=2), n_step=120,
+        date_train_from='2017-01-01', offset='3M',
         max_process_count=2
     )
+
+
+if __name__ == '__main__':
+    pass
+    _test_train_on_each_period()
     # _test_train_round_iter_func(round_n_per_target_day=2)
