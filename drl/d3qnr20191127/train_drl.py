dccdc8dfac14e8552b75401ca5dd5b395afe3729
mmmaaaggg <mmmaaaggg@163.com>
2019-12-17 22:01:06 +0800

    opt param

--- drl/d3qnr20191127/train_drl.py
+++ drl/d3qnr20191127/train_drl.py
@@ -31,11 +31,11 @@ def train_round_iter_func(round_n_per_target_day=2, target_avg_holding_days=[5, 
     for round_n_sub in range(round_n_per_target_day):
         for days in target_avg_holding_days:
             env_kwargs = dict(state_with_flag=True, fee_rate=0.001)
-            agent_kwargs = dict(keep_last_action=math.pow(0.5, 1 / days), batch_size=512,
+            agent_kwargs = dict(keep_last_action=math.pow(0.5, 1 / days), batch_size=128,
                                 epsilon_memory_size=10, random_drop_best_cache_rate=0.01)
             num_episodes = 3000 + 200 * round_n_sub
             train_kwargs = dict(round_n=round_n, num_episodes=num_episodes, n_episode_pre_record=num_episodes // 8,
-                                model_name=MODEL_NAME, get_agent_func=get_agent)
+                                model_name=MODEL_NAME, get_agent_func=get_agent, output_reward_csv=True)
             yield round_n, env_kwargs, agent_kwargs, train_kwargs
             round_n += 1
 
@@ -62,9 +62,9 @@ def _test_train_on_each_period():
         md_loader_func=lambda range_to=None: load_data(
             'RB.csv', folder_path=DATA_FOLDER_PATH, index_col='trade_date', range_to=range_to)[OHLCAV_COL_NAME_LIST],
         get_factor_func=get_factor_func,
-        train_round_kwargs_iter_func=functools.partial(train_round_iter_func, round_n_per_target_day=3), n_step=60,
+        train_round_kwargs_iter_func=functools.partial(train_round_iter_func, round_n_per_target_day=4), n_step=60,
         date_train_from='2017-01-01', offset='3M',
-        use_pool=True, max_process_count=2,
+        use_pool=False, max_process_count=2,
         date_period_count=1,  # None 如果需要训练全部日期
     )
 
