769f5c8645aef3cc831d643058489bb86f7c94a0
mmmaaaggg <mmmaaaggg@163.com>
2020-01-03 18:09:36 +0800

    num_episodes = 2000 + 200 * round_n_sub

--- drl/d3qnr20200101/train_drl.py
+++ drl/d3qnr20200101/train_drl.py
@@ -32,9 +32,9 @@ def train_round_iter_func(round_n_per_target_day=2, target_avg_holding_days=[3, 
                 sin_step=np.pi/50, epsilon_decay=0.993, epsilon_min=0.01, epsilon_sin_max=0.1,
                 build_model_layer_count=4, train_net_period=10, keep_epsilon_init_4_first_n=50,
             )
-            num_episodes = 2000 + 200 * round_n_sub
+            num_episodes = 3000 + 200 * round_n_sub
             train_kwargs = dict(round_n=round_n, num_episodes=num_episodes, n_episode_pre_record=num_episodes // 8,
-                                model_name=MODEL_NAME, get_agent_func=get_agent, output_reward_csv=True,
+                                model_name=MODEL_NAME, get_agent_func=get_agent, output_reward_csv=False,
                                 available_check_after_n_episode=2000)
             yield round_n, env_kwargs, agent_kwargs, train_kwargs
             round_n += 1
