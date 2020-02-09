30d48eb5aa7bd7527697df42f8127072760556a8
mmmaaaggg <mmmaaaggg@163.com>
2020-02-09 21:07:36 +0800

    add env market modules

--- dr2/dqn20200209/train/env.py
+++ dr2/dqn20200209/train/env.py
@@ -7,6 +7,104 @@
 @desc    :
 环境信息，此模块稍后将被移植入 ibats_commons
 """
+import pandas as pd
+import numpy as np
+from tf_agents.environments.py_environment import PyEnvironment
+from tf_agents.specs import array_spec
+from tf_agents.trajectories import time_step as ts
+from dr2.dqn20200209.train.market import QuotesMarket
+
+ACTION_LONG, ACTION_SHORT, ACTION_CLOSE, ACTION_KEEP = 0, 1, 2, 3
+ACTIONS = [ACTION_LONG, ACTION_SHORT, ACTION_CLOSE, ACTION_KEEP]
+ACTION_OP_LONG, ACTION_OP_SHORT, ACTION_OP_CLOSE, ACTION_OP_KEEP = 'long', 'short', 'close', 'keep'
+ACTION_OPS = [ACTION_OP_LONG, ACTION_OP_SHORT, ACTION_OP_CLOSE, ACTION_OP_KEEP]
+ACTION_OP_DIC = {_: ACTION_OPS[_] for _ in ACTIONS}
+OP_ACTION_DIC = {ACTION_OPS[_]: _ for _ in ACTIONS}
+# 内部访问的flag
+_FLAG_LONG, _FLAG_SHORT, _FLAG_EMPTY = 1, -1, 0
+# one-hot 模式的flag
+FLAG_LONG, FLAG_SHORT, FLAG_EMPTY = _FLAG_LONG + 1, _FLAG_SHORT + 1, _FLAG_EMPTY + 1
+FLAGS = [FLAG_LONG, FLAG_SHORT, FLAG_EMPTY]
+
+
+class AccountEnv(PyEnvironment):
+
+    def __init__(self, md_df: pd.DataFrame, data_factors: np.ndarray,
+                 state_with_flag=True, action_kind_count=2, **kwargs):
+        super(AccountEnv, self).__init__()
+        kwargs['state_with_flag'] = state_with_flag
+        self.market = QuotesMarket(md_df, data_factors, **kwargs)
+        self._state_spec = array_spec.ArraySpec(
+            shape=data_factors.shape[1:], dtype=np.float32, name='state')
+        self._flag_spec = array_spec.BoundedArraySpec(
+            shape=(1,), dtype=np.float32, name='flag', minimum=0.0, maximum=1.0)
+        self._action_spec = array_spec.BoundedArraySpec(
+            shape=(1,), dtype=np.int32, name='action', minimum=0.0, maximum=max(ACTIONS[:action_kind_count]))
+        if state_with_flag:
+            self._observation_spec = {'state': self._state_spec, 'flag': self._flag_spec}
+        else:
+            self._observation_spec = self._state_spec
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    def action_spec(self):
+        return self._action_spec
+
+    def get_info(self):
+        observation, rewards, done = self.market.step_ret_latest
+        if done:
+            return ts.termination(observation, rewards)
+        else:
+            return ts.transition(observation, rewards)
+
+    def _step(self, action):
+        observation, rewards, done = self.market.step(action)
+        if done:
+            return ts.termination(observation, rewards)
+        else:
+            return ts.transition(observation, rewards)
+
+    def _reset(self):
+        return ts.transition(self.market.reset(), 0.0)
+
+
+def _get_df():
+    n_step = 60
+    ohlcav_col_name_list = ["open", "high", "low", "close", "amount", "volume"]
+    from ibats_common.example.data import load_data
+    md_df = load_data('RB.csv', folder_path='/home/mg/github/IBATS_Common/ibats_common/example/data'
+                      ).set_index('trade_date')[ohlcav_col_name_list]
+    md_df.index = pd.DatetimeIndex(md_df.index)
+    from ibats_common.backend.factor import get_factor, transfer_2_batch
+    factors_df = get_factor(md_df, dropna=True)
+    df_index, df_columns, data_arr_batch = transfer_2_batch(factors_df, n_step=n_step)
+    md_df = md_df.loc[df_index, :]
+    return md_df[['close', 'open']], data_arr_batch
+
+
+def account_env_test():
+    from tf_agents.policies.random_py_policy import RandomPyPolicy
+    from tf_agents.drivers.dynamic_episode_driver import DynamicEpisodeDriver
+    from tf_agents.metrics.py_metrics import AverageReturnMetric
+    from tf_agents.drivers.py_driver import PyDriver
+
+    md_df, data_factors = _get_df()
+    env = AccountEnv(md_df=md_df, data_factors=data_factors)
+    policy = RandomPyPolicy(time_step_spec=env.time_step_spec(), action_spec=env.action_spec())
+    replay_buffer = []
+    metric = AverageReturnMetric()
+    observers = [replay_buffer.append, metric]
+    driver = PyDriver(
+        env, policy, observers, max_steps=1000, max_episodes=10)
+    initial_time_step = env.reset()
+    final_time_step, _ = driver.run(initial_time_step)
+    print('Replay Buffer:')
+    for traj in replay_buffer:
+        print(traj)
+
+    print('Average Return: ', metric.result())
+
 
 if __name__ == "__main__":
-    pass
+    account_env_test()
---
+++ dr2/dqn20200209/train/market.py
@@ -0,0 +1,218 @@
+#! /usr/bin/env python3
+"""
+@author  : MG
+@Time    : 2020/2/9 下午2:06
+@File    : market.py
+@contact : mmmaaaggg@163.com
+@desc    : 从 /ibats_common/backend/rl/emulator/market2.py 迁移来
+"""
+import numpy as np
+import pandas as pd
+
+ACTION_LONG, ACTION_SHORT, ACTION_CLOSE, ACTION_KEEP = 0, 1, 2, 3
+ACTIONS = [ACTION_LONG, ACTION_SHORT, ACTION_CLOSE, ACTION_KEEP]
+ACTION_OP_LONG, ACTION_OP_SHORT, ACTION_OP_CLOSE, ACTION_OP_KEEP = 'long', 'short', 'close', 'keep'
+ACTION_OPS = [ACTION_OP_LONG, ACTION_OP_SHORT, ACTION_OP_CLOSE, ACTION_OP_KEEP]
+ACTION_OP_DIC = {_: ACTION_OPS[_] for _ in ACTIONS}
+OP_ACTION_DIC = {ACTION_OPS[_]: _ for _ in ACTIONS}
+# 内部访问的flag
+_FLAG_LONG, _FLAG_SHORT, _FLAG_EMPTY = 1, -1, 0
+# one-hot 模式的flag
+FLAG_LONG, FLAG_SHORT, FLAG_EMPTY = _FLAG_LONG + 1, _FLAG_SHORT + 1, _FLAG_EMPTY + 1
+FLAGS = [FLAG_LONG, FLAG_SHORT, FLAG_EMPTY]
+
+
+class QuotesMarket(object):
+    def __init__(self, md_df: pd.DataFrame, data_factors, init_cash=2e5, fee_rate=3e-3,
+                 state_with_flag=False, reward_with_fee0=False, md_close_label='close', md_open_label='open'):
+        self.data_close = md_df[md_close_label]
+        self.data_open = md_df[md_open_label]
+        self.data_factor = data_factors
+        self.action_operations = ACTION_OPS
+        self.fee_rate = fee_rate  # 千三手续费
+        self.fee_curr_step = 0
+        self.fee_tot = 0
+        self.max_step_count = self.data_factor.shape[0] - 1
+        self.init_cash = init_cash
+        # reset use
+        self.step_counter = 0
+        self.cash = self.init_cash
+        self.position = 0
+        self.total_value = self.cash + self.position
+        self.total_value_fee0 = self.cash + self.position
+        self._flag = _FLAG_EMPTY
+        self.state_with_flag = state_with_flag
+        self.reward_with_fee0 = reward_with_fee0
+        self.action_count = 0
+        self._observation_latest = None
+        self._reward_latest = 0.0
+        self._done = False
+        self.step_ret_latest = None
+
+    @property
+    def flag(self):
+        """外部访问 flag 标志位，为了方便 one_hot 模式，因此做 + 1 处理"""
+        return self._flag + 1  # 为了方便转换成 one_hot 模式的 flag
+
+    def reset(self):
+        self.step_counter = 0
+        self.cash = self.init_cash
+        self.position = 0
+        self.total_value = self.cash + self.position
+        self.total_value_fee0 = self.cash + self.position
+        self._flag = _FLAG_EMPTY
+        self.fee_curr_step = 0
+        self.fee_tot = 0
+        self.action_count = 0
+        if self.state_with_flag:
+            self._observation_latest = {'state': self.data_factor[self.step_counter], 'flag': np.array([self.flag])}
+        else:
+            self._observation_latest = self.data_factor[self.step_counter]
+        self._reward_latest = 0.0
+        self._done = False
+        self.step_ret_latest = self._observation_latest, self._reward_latest, self._done
+        return self._observation_latest
+
+    def observation_latest(self):
+        return self._observation_latest
+
+    def get_action_operations(self):
+        return self.action_operations
+
+    def long(self):
+        self._flag = _FLAG_LONG
+        quotes = self.data_open[self.step_counter] * 10
+        self.cash -= quotes * (1 + self.fee_rate)
+        self.position = quotes
+        self.fee_curr_step += quotes * self.fee_rate
+        self.action_count += 1
+
+    def short(self):
+        self._flag = _FLAG_SHORT
+        quotes = self.data_open[self.step_counter] * 10
+        self.cash += quotes * (1 - self.fee_rate)
+        self.position = - quotes
+        self.fee_curr_step += quotes * self.fee_rate
+        self.action_count += 1
+
+    def keep(self):
+        quotes = self.data_open[self.step_counter] * 10
+        self.position = quotes * self._flag
+
+    def close_long(self):
+        self._flag = _FLAG_EMPTY
+        quotes = self.data_open[self.step_counter] * 10
+        self.cash += quotes * (1 - self.fee_rate)
+        self.position = 0
+        self.fee_curr_step += quotes * self.fee_rate
+        self.action_count += 1
+
+    def close_short(self):
+        self._flag = _FLAG_EMPTY
+        quotes = self.data_open[self.step_counter] * 10
+        self.cash -= quotes * (1 + self.fee_rate)
+        self.position = 0
+        self.fee_curr_step += quotes * self.fee_rate
+        self.action_count += 1
+
+    def step(self, action: int):
+        self.fee_curr_step = 0
+        if action == ACTION_LONG:
+            if self._flag == _FLAG_EMPTY:
+                self.long()
+            elif self._flag == _FLAG_SHORT:
+                self.close_short()
+                self.long()
+            else:
+                self.keep()
+
+        elif action == ACTION_CLOSE:
+            if self._flag == _FLAG_LONG:
+                self.close_long()
+            elif self._flag == _FLAG_SHORT:
+                self.close_short()
+            else:
+                pass
+
+        elif action == ACTION_SHORT:
+            if self._flag == _FLAG_EMPTY:
+                self.short()
+            elif self._flag == _FLAG_LONG:
+                self.close_long()
+                self.short()
+            else:
+                self.keep()
+        elif action == ACTION_KEEP:
+            self.keep()
+        else:
+            raise ValueError(f"action={action} should be one of keys {ACTIONS}")
+
+        # 计算费用
+        self.fee_tot += self.fee_curr_step
+        self.total_value_fee0 = self.total_value + self.fee_tot
+
+        # 计算价值
+        price = self.data_close[self.step_counter]
+        position = price * 10 * self._flag
+        reward = self.cash + position - self.total_value
+        self.step_counter += 1
+        self.total_value = position + self.cash
+        next_observation = self.data_factor[self.step_counter]
+
+        if self.total_value < price:
+            self._done = True
+        if self.step_counter >= self.max_step_count:
+            self._done = True
+
+        self._observation_latest = {'state': next_observation, 'flag': np.array([self.flag])}\
+            if self.state_with_flag else next_observation
+        self._reward_latest = (reward / price, (reward + self.fee_curr_step) / price) if self.reward_with_fee0 else (
+                reward / price)
+        self.step_ret_latest = self._observation_latest, self._reward_latest, self._done
+        return self.step_ret_latest
+
+
+def _test_quote_market():
+    n_step = 60
+    ohlcav_col_name_list = ["open", "high", "low", "close", "amount", "volume"]
+    from ibats_common.example.data import load_data
+    md_df = load_data('RB.csv', folder_path='/home/mg/github/IBATS_Common/ibats_common/example/data'
+                      ).set_index('trade_date')[ohlcav_col_name_list]
+    md_df.index = pd.DatetimeIndex(md_df.index)
+    from ibats_common.backend.factor import get_factor, transfer_2_batch
+    factors_df = get_factor(md_df, dropna=True)
+    df_index, df_columns, data_arr_batch = transfer_2_batch(factors_df, n_step=n_step)
+    md_df = md_df.loc[df_index, :]
+    # 建立 QuotesMarket
+    qm = QuotesMarket(md_df=md_df[['close', 'open']], data_factors=data_arr_batch, state_with_flag=True)
+    next_observation = qm.reset()
+    assert len(next_observation) == 2
+    assert next_observation['state'].shape[0] == n_step
+    assert next_observation['flag'] == FLAG_EMPTY
+    next_observation, reward, done = qm.step(ACTION_LONG)
+    assert len(next_observation) == 2
+    assert next_observation['flag'] == FLAG_LONG
+    assert not done
+    next_observation, reward, done = qm.step(ACTION_CLOSE)
+    assert next_observation['flag'] == FLAG_EMPTY
+    assert reward != 0
+    next_observation, reward, done = qm.step(ACTION_CLOSE)
+    assert next_observation['flag'] == FLAG_EMPTY
+    assert reward == 0
+    next_observation, reward, done = qm.step(ACTION_KEEP)
+    assert next_observation['flag'] == FLAG_EMPTY
+    assert reward == 0
+    next_observation, reward, done = qm.step(ACTION_SHORT)
+    assert next_observation['flag'] == FLAG_SHORT
+    assert not done
+    next_observation, reward, done = qm.step(ACTION_KEEP)
+    assert next_observation['flag'] == FLAG_SHORT
+    assert reward != 0
+    try:
+        qm.step(4)
+    except ValueError:
+        print('is ok for not supporting action>3')
+
+
+if __name__ == "__main__":
+    _test_quote_market()
