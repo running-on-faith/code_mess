b0955b9362c1186013e6248404330d6bc208244b
mmmaaaggg <mmmaaaggg@163.com>
2020-06-18 21:51:15 +0800

    feature: add epsilon_greedy param

--- dr2/dqn20200209/train/agent.py
+++ dr2/dqn20200209/train/agent.py
@@ -15,7 +15,7 @@ from dr2.dqn20200209.train.network import get_network
 logger = logging.getLogger()
 
 
-def get_agent(env, state_with_flag=False):
+def get_agent(env, state_with_flag=False, epsilon_greedy=0.1):
     # from tf_agents.agents import DqnAgent
     from tf_agents.agents.dqn.dqn_agent import DdqnAgent
     network, optimizer, loss_fn = get_network(
@@ -26,6 +26,7 @@ def get_agent(env, state_with_flag=False):
         env.action_spec(),
         q_network=network,
         optimizer=optimizer,
+        epsilon_greedy=epsilon_greedy,
         td_errors_loss_fn=loss_fn,
         train_step_counter=global_step,
     )
