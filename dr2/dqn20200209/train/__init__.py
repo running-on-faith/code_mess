5bc22ab7b94e63f00fd72e7e84656aa9744c6f33
mmmaaaggg <mmmaaaggg@163.com>
2020-06-27 18:26:28 +0800

    feature: plot_modal_2_file

--- dr2/dqn20200209/train/__init__.py
+++ dr2/dqn20200209/train/__init__.py
@@ -7,5 +7,15 @@
 @desc    : 
 """
 
+
+def plot_modal_2_file(model, file_name, auto_open=True):
+    from keras.utils import plot_model
+    from ibats_utils.mess import open_file_with_system_app
+    file_path = file_name
+    plot_model(model, to_file=file_path, show_shapes=True)
+    if auto_open:
+        open_file_with_system_app(file_path)
+
+
 if __name__ == "__main__":
     pass
