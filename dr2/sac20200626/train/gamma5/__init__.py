b9db1b0bb49979d45669a533242609496766eff9
mmmaaaggg <mmmaaaggg@163.com>
2020-06-28 22:33:39 +0800

    feature: gamma=0.5

--- dr2/sac20200626/train/gamma5/__init__.py
+++ dr2/sac20200626/train/gamma5/__init__.py
@@ -1,14 +1,13 @@
 #! /usr/bin/env python3
 """
 @author  : MG
-@Time    : 2020/6/27 下午6:35
+@Time    : 2020/6/28 下午10:25
 @File    : __init__.py.py
 @contact : mmmaaaggg@163.com
 @desc    : 
 """
 from dr2.sac20200626.train.train_drl import train_drl
 
-train_drl(train_loop_count=300, num_collect_episodes=10, agent_kwargs={"gamma": 0.9})
-
 if __name__ == "__main__":
-    pass
+    train_drl(train_loop_count=200, num_collect_episodes=10, train_count_per_loop=50, agent_kwargs={"gamma": 0.5})
+
