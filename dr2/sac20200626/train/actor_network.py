ca114fd6097aa75daea0ad3acd0c8c39e1cbdd80
马过 <mmmaaaggg@163.com>
2020-06-30 11:24:28 +0800

    feature: multi layers on state

--- dr2/sac20200626/train/actor_network.py
+++ dr2/sac20200626/train/actor_network.py
@@ -12,6 +12,8 @@ from tensorflow.keras.layers import LSTM, Lambda, Concatenate, Dense, Dropout
 from tensorflow.keras import Sequential
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
 from tf_agents.networks.actor_distribution_network import ActorDistributionNetwork
+from tf_agents.networks.sequential_layer import SequentialLayer
+
 from dr2.dqn20200209.train import plot_modal_2_file
 
 logger = logging.getLogger()
@@ -22,33 +24,30 @@ def get_actor_network(env: TFPyEnvironment, state_with_flag: bool, activation_fn
     state_spec = observation_spec[0]
     input_shape = state_spec.shape[-1]
     unit = input_shape * 2
-    _state_layer = LSTM(
+    _state_layer = SequentialLayer([LSTM(
         unit,
         dropout=0.2,
-        recurrent_dropout=0.2,
-    )
+        recurrent_dropout=0.2, ),
+        Dense(input_shape // 2, activation=activation_fn),
+        Dropout(0.2),
+        Dense(input_shape // 4, activation=activation_fn),
+        Dropout(0.2),
+        Dense(input_shape // 8, activation=activation_fn),
+        Dropout(0.2),
+        Dense(input_shape // 16, activation=activation_fn),
+        Dropout(0.2),
+        Dense(action_spec.shape.num_elements(), activation=activation_fn),
+    ])
     logger.debug("actor_network LSTM unit:%d", unit)
     _flag_layer = Lambda(lambda x: x)
     _rr_layer = Lambda(lambda x: x)
     preprocessing_layers = [_state_layer, _flag_layer, _rr_layer]
-
-    seq_layer = Sequential()
-    seq_layer.add(Dense(input_shape // 2, activation=activation_fn))
-    seq_layer.add(Dropout(0.2))
-    seq_layer.add(Dense(input_shape // 4, activation=activation_fn))
-    seq_layer.add(Dropout(0.2))
-    seq_layer.add(Dense(input_shape // 8, activation=activation_fn))
-    seq_layer.add(Dropout(0.2))
-    seq_layer.add(Dense(input_shape // 16, activation=activation_fn))
-    seq_layer.add(Dropout(0.2))
-    seq_layer.add(Dense(action_spec.shape.num_elements(), activation=activation_fn))
 
     net = ActorDistributionNetwork(
         input_tensor_spec=observation_spec,
         output_tensor_spec=action_spec,
         preprocessing_layers=preprocessing_layers,
         preprocessing_combiner=Concatenate(axis=-1),
-        continuous_projection_net=seq_layer
     )
     # plot_modal_2_file(net, 'actor.png')
     return net
