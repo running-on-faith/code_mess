183052a6b6d518265862c7974f3f57510efe7c1f
mmmaaaggg <mmmaaaggg@163.com>
2020-07-02 00:05:11 +0800

    feature: exam2.py

--- stock/exam2.py
+++ stock/exam2.py
@@ -8,10 +8,8 @@ import matplotlib.pyplot as plt
 from collections import OrderedDict, defaultdict
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
-DIR_PATH = r'C:\Users\26559\Downloads\弗居投资笔试 20200630\Data'
-
-
-# DIR_PATH = r'/home/mg/Downloads/Data'
+# DIR_PATH = r'C:\Users\26559\Downloads\弗居投资笔试 20200630\Data'
+DIR_PATH = r'/home/mg/Downloads/Data'
 
 
 def get_df(dir_path=r'/home/mg/Downloads/Data'):
@@ -21,11 +19,59 @@ def get_df(dir_path=r'/home/mg/Downloads/Data'):
     return data_df
 
 
-if __name__ == '__main__':
+def factor_adf(price_df):
+    corr_dic = {}
+    for n in [5]:
+        factor_df = (price_df / price_df.shift(n) - 1).dropna()
+        corr_s = pd.Series(np.diag(factor_df.T.corr(), 1), index=factor_df.index[1:])
+        corr_dic[f"mom_{n}"] = corr_s
+        corr_dic[f"mom_{n} Avg"] = corr_s.rolling(10).mean()
+
+    corr_df = pd.DataFrame(corr_dic).dropna()
+    """
+    plot图可以看出,
+    """
+    corr_df.plot()
+
+
+def factor_ic(price_df):
+    corr_dic = {}
+    for n in [5, 10, 20]:
+        factor_df = (price_df / price_df.shift(n) - 1).dropna()
+        rr_df = (price_df.shift(-1) / price_df - 1).dropna()
+        idxs = factor_df.index & rr_df.index
+        factor_df = factor_df.loc[idxs, :]
+        rr_df = rr_df.loc[idxs, :]
+        corr_dic[f"mom_{n}"] = {idx: factor_df.loc[idx].corr(rr_df.loc[idx]) for idx in idxs}
+
+    corr_df = pd.DataFrame(corr_dic).dropna()
+    corr_df.plot(grid=True)
+    plt.savefig("factor_ic.png")
+    plt.close()
+
+
+def mean_reversion(price_df):
+    n = 5
+    mean_df = price_df.rolling(n).mean()
+    std_df = price_df.rolling(n).std()
+    diff_df = ((price_df - mean_df) / std_df).dropna()
+    mean_average_reversion = diff_df.std().mean()
+    return mean_average_reversion
+
+
+def answer():
     data_df = get_df(DIR_PATH)
-    price_df = (data_df/100 + 1).cumprod()
-    factor_df = (price_df - price_df.shift(10)).dropna()
+    price_df = (data_df / 100 + 1).cumprod()
     price_df.quantile([0.1, 0.25, 0.5, 0.75, 0.9], axis=1).T.plot(title='Quantile Returns')
     plt.savefig('Quantile Returns.png')
     plt.close()
-    factor = (data_df.rolling(10).mean() - data_df).dropna()
+    # adf
+    factor_adf(price_df)
+    # 因子相关性
+    factor_ic(price_df)
+    # plt.close('all')
+    mean_reversion(price_df)
+
+
+if __name__ == '__main__':
+    answer()
