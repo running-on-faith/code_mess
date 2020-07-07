09be1b91aef8885b223420301bd1e3062e1bdaa7
mmmaaaggg <mmmaaaggg@163.com>
2020-07-07 22:25:56 +0800

    feature: train_count_per_loop=100

--- dr2/dqn20200209/train/20200704_1f4344fb_gamma8/__init__.py
+++ dr2/dqn20200209/train/20200704_1f4344fb_gamma8/__init__.py
@@ -12,5 +12,9 @@ if __name__ == "__main__":
     epsilon_greedy = 0.1
     gamma = 0.8
     # num_collect_episodes 被默认设置为 epsilon_greedy 倒数的 2 背,以确保又足够的样板,防止由于随机随机策略而导致价值计算失衡
-    train_drl(train_loop_count=200, num_collect_episodes=int(2 / epsilon_greedy),
-              epsilon_greedy=epsilon_greedy, gamma=gamma)
+    train_drl(train_loop_count=200,
+              num_collect_episodes=int(2 / epsilon_greedy),
+              epsilon_greedy=epsilon_greedy,
+              train_sample_batch_size=1024,
+              train_count_per_loop=100,
+              gamma=gamma)
