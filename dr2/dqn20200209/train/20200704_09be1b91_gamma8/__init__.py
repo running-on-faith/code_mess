ec6a37520e83348ee3138c284091f96fa7aff6d6
mmmaaaggg <mmmaaaggg@163.com>
2020-07-08 22:11:57 +0800

    feature: lower train count

--- dr2/dqn20200209/train/20200704_09be1b91_gamma8/__init__.py
+++ dr2/dqn20200209/train/20200704_09be1b91_gamma8/__init__.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     gamma = 0.8
     # num_collect_episodes 被默认设置为 epsilon_greedy 倒数的 2 背,以确保又足够的样板,防止由于随机随机策略而导致价值计算失衡
     train_drl(train_loop_count=200,
-              num_collect_episodes=int(2 / epsilon_greedy),
+              num_collect_episodes=int(1 / epsilon_greedy),
               epsilon_greedy=epsilon_greedy,
               train_sample_batch_size=1024,
-              train_count_per_loop=100,
+              train_count_per_loop=50,
               gamma=gamma)
