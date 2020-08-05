0dcdb3c4cefa0d3fa3d682680ea857a295be67d2
mmmaaaggg <mmmaaaggg@163.com>
2020-08-05 23:14:58 +0800

    feature: lstm_conv5_epsilon_greedy15_gamma5_0dc8faa9

--- dr2/dqn20200209/train/20200805_3e3ce6b6/train_conv_layer5.py
+++ dr2/dqn20200209/train/20200805_3e3ce6b6/train_conv_layer5.py
@@ -76,5 +76,5 @@ if __name__ == "__main__":
         train_count_per_loop=50,
         gamma=gamma,
         network_kwargs_func=network_kwargs_func,
-        base_path='lstm_two_conv_epsilon_greedy15_gamma5_cabd847e'
+        base_path='lstm_conv5_epsilon_greedy15_gamma5_0dc8faa9'
     )
