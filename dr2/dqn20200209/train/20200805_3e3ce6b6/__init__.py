85677d3e1632744cd5330da4515dd5856df0d035
mmmaaaggg <mmmaaaggg@163.com>
2020-08-05 22:42:52 +0800

    feature: lstm_two_conv_epsilon_greedy15_gamma5_cabd847e

---
+++ dr2/dqn20200209/train/20200805_3e3ce6b6/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+"""
+@author  : MG
+@Time    : 2020/8/5 下午10:35
+@File    : __init__.py.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+
+if __name__ == "__main__":
+    pass
