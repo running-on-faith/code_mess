3354a295270c709da2ad068a11e68ed0aa399e36
mmmaaaggg <mmmaaaggg@163.com>
2020-08-25 14:26:55 +0800

    add more test case

---
+++ dr2/dqn20200209/train/train_20200825_84224e73_reproduce_20200623_517fb234_2/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+"""
+@author  : MG
+@Time    : 2020/8/25 下午2:24
+@File    : __init__.py.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+
+if __name__ == "__main__":
+    pass
