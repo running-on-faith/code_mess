e527892ca533a0315056c93d0fb598688ea0447e
mmmaaaggg <mmmaaaggg@163.com>
2020-09-07 14:13:13 +0800

    add DoubleThresholdWithinPeriodsBSStrategy, generate_available_period

--- params_optimizer/optimizer.py
+++ params_optimizer/optimizer.py
@@ -70,6 +70,10 @@ class ZeroLineBSStrategy(SimpleStrategy):
 
 
 class DoubleThresholdBSStrategy(SimpleStrategy):
+    """
+    上下界买卖策略
+    """
+
     def __init__(self, buy_line, sell_line, lower_buy_higher_sell=True,
                  compare_index=0, mid_zone_action=ACTION_KEEP):
         """
@@ -107,10 +111,53 @@ class DoubleThresholdBSStrategy(SimpleStrategy):
         return action
 
 
+class DoubleThresholdWithinPeriodsBSStrategy(DoubleThresholdBSStrategy):
+    """
+    带有效日期范围的上下界买卖策略
+    """
+
+    def __init__(self, buy_line, sell_line, periods, lower_buy_higher_sell=True,
+                 compare_index=1, mid_zone_action=ACTION_KEEP, date_index=0):
+        super(DoubleThresholdWithinPeriodsBSStrategy, self).__init__(
+            buy_line, sell_line, lower_buy_higher_sell,
+            compare_index, mid_zone_action
+        )
+        self.periods = np.array(periods, dtype='M')
+        assert self.periods.shape[1] == 2, 'periods 日期区间必须是一对日期'
+        self.date_index = date_index
+        self.periods_available = ~np.isnat(self.periods)
+
+    def run(self, factors: np.ndarray) -> int:
+        # 当前 bar 日期
+        cur_date = factors[self.date_index]
+        # 判断日期是否在有效日期区间范围内，
+        # 如果不在，直接平仓，
+        # 如果在，按照上下界进行判断
+        is_ok = False
+        for (date_from, date_to), (date_from_ok, date_to_ok) in zip(self.periods, self.periods_available):
+            is_ok_from = (not date_from_ok) or date_from <= cur_date
+            is_ok_to = (not date_to_ok) or cur_date <= date_to
+            if is_ok_from and is_ok_to:
+                is_ok = True
+                break
+
+        if not is_ok:
+            action = ACTION_CLOSE
+        else:
+            action = super().run(factors)
+
+        return action
+
+
 def _test_optimizer():
+    """
+    以上下界买卖策略为例测试程序是否可以正常运行
+    """
+
     def factor_generator(df: pd.DataFrame, short=12, long=26, signal=9):
         import talib
         close_s = df['close']
+        dates = df.index.to_numpy()
         dif, dea, macd = talib.MACD(close_s, fastperiod=short, slowperiod=long, signalperiod=signal)
         factors = np.expand_dims(macd, axis=1)
         return factors
@@ -121,7 +168,86 @@ def _test_optimizer():
         md_df,
         factor_generator=factor_generator,
         params_kwargs_iter=[{
-            "strategy_kwargs": {"buy_line": 80, "sell_line": 20},
+            "strategy_kwargs": {"buy_line": 50, "sell_line": -50},
+            "factor_kwargs": {"short": 12, "long": 26, "signal": 9}
+        }],
+        date_from=date_from, date_to=date_to)
+
+    data_4_shown = []
+    for key, result_dic in result_dic.items():
+        print('\n', key, ':\n')
+        dic = json.loads(key)
+        data_dic = dic['factor_kwargs']
+        data_dic['calmar'] = result_dic['nav_stats'].calmar
+        data_4_shown.append(data_dic)
+        for name, item in result_dic.items():
+            print('\t', name)
+            print('\t', item)
+
+    print("data_4_shown:")
+    for _ in data_4_shown:
+        print(_)
+
+
+def generate_available_period(contract_month: int, date_from_str: str, date_to_str: str) -> list:
+    """
+    生成合约对应的有效日期范围，与给点日期范围的交集
+    该功能仅用于策略回测是对1月份连续合约等连续合约数据是使用
+    根据合约生成日期范围规则，例如：
+    1月合约，上一年8月1 ~ 11月31日
+    5月合约，上一年12月1 ~ 3月31日
+    9月合约，4月1日 ~ 7月31日
+    """
+    date_from = pd.to_datetime(date_from_str if date_from_str is not None else '2000-01-01')
+    date_to = pd.to_datetime(date_to_str if date_from_str is not None else '2030-01-01')
+    periods = []
+    for range_year in range(date_from.year, date_to.year + 2):
+        year, month = (range_year, contract_month - 5) if contract_month > 5 else (
+            range_year - 1, contract_month + 12 - 5)
+        range_from = pd.to_datetime(f"{year:4d}-{month:02d}-01")
+        year, month = (range_year, contract_month - 1) if contract_month > 1 else (
+            range_year - 1, 12)
+        range_to = pd.to_datetime(f"{year:4d}-{month:02d}-01") - pd.to_timedelta(1, unit='D')
+        # 与 date_from date_to 取交集
+        if range_to < date_from or date_to < range_from:
+            continue
+        range_from = date_from if range_from < date_from < range_to else range_from
+        range_to = date_to if range_from < date_to < range_to else range_to
+        periods.append([str(range_from.date()), str(range_to.date())])
+
+    return periods
+
+
+def _test_generate_available_period():
+    date_from, date_to = '2013-01-01', '2018-10-31'
+    contract_month = 1
+    periods = generate_available_period(contract_month, date_from, date_to)
+    print(periods)
+
+
+def _test_optimizer2():
+    """
+    以带日期范围的上下界买卖策略为例测试程序是否可以正常运行
+    """
+
+    def factor_generator(df: pd.DataFrame, short=12, long=26, signal=9):
+        import talib
+        close_s = df['close']
+        dates = df.index.to_numpy()
+        dif, dea, macd = talib.MACD(close_s, fastperiod=short, slowperiod=long, signalperiod=signal)
+        # factors = np.expand_dims(macd, axis=1)
+        factors = np.array([_ for _ in zip(dates, macd)])
+        return factors
+
+    date_from, date_to = '2013-01-01', '2018-12-31'
+    md_df = load_md(instrument_type='RB')
+    contract_month = 1
+    periods = generate_available_period(contract_month, date_from, date_to)
+    result_dic = DoubleThresholdWithinPeriodsBSStrategy.bulk_run_on_range(
+        md_df,
+        factor_generator=factor_generator,
+        params_kwargs_iter=[{
+            "strategy_kwargs": {"buy_line": 50, "sell_line": -50, 'periods': periods},
             "factor_kwargs": {"short": 12, "long": 26, "signal": 9}
         }],
         date_from=date_from, date_to=date_to)
@@ -143,4 +269,6 @@ def _test_optimizer():
 
 
 if __name__ == "__main__":
-    _test_optimizer()
+    # _test_optimizer()
+    # _test_generate_available_period()
+    _test_optimizer2()
